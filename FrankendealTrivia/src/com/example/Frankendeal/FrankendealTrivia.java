463d64acbbfcb3e463bc37773dc1dbae9dbb2338
Jen C <work@icismachine.com>
2010-10-19 20:02:58 -0700

    updated android app that sends location lon/lat

--- FrankendealTrivia/src/com/example/Frankendeal/FrankendealTrivia.java
+++ FrankendealTrivia/src/com/example/Frankendeal/FrankendealTrivia.java
@@ -2,15 +2,13 @@ package com.example.Frankendeal;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.ActivityInfo;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 /*
 public class FrankendealTrivia extends Activity implements GeolocationPermissions.Callback{
@@ -67,21 +65,124 @@ public class FrankendealTrivia extends Activity implements GeolocationPermission
 }
 */
 
+//http://efreedom.com/Question/1-3767155/Getting-GPS-Location-Load-Another-URL
+public class FrankendealTrivia extends Activity implements LocationListener{
+
+    private WebView browserView;
+     private static String PROVIDER="gps";
+     private static final String LOG_TAG = "FrankenDeal";
+     private LocationManager myLocationManager=null;
+     private String url;
+     private boolean LocFound = false;
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+
+         //Removes the title bar in the application
+         requestWindowFeature(Window.FEATURE_NO_TITLE);
+         setContentView(R.layout.main);
+
+         //Creation of the Webview found in the XML Layout file
+         browserView = (WebView)findViewById(R.id.webview); //webkit
+
+         //Removes both vertical and horizontal scroll bars 
+         browserView.setVerticalScrollBarEnabled(false);
+         browserView.setHorizontalScrollBarEnabled(false);
+
+
+         myLocationManager=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+         //Enable Javascripts
+
+         url = "http://frankendeal.com/logEvent.php";//"http://miniapps.co.uk/geometer/";
+
+         browserView.getSettings().setJavaScriptEnabled(true);
+
+
+         //The website which is wrapped to the webview
+
+        browserView.loadUrl(url);
+
+
+          }
+
+    @Override
+    public void onResume() {
+    super.onResume();
+    myLocationManager.requestLocationUpdates(PROVIDER, 0,
+    0,
+    this);
+    }
+
+    @Override
+    public void onPause() {
+    super.onPause();
+    myLocationManager.removeUpdates(this);
+    }
+
+    public void locationFound(){
+
+        myLocationManager.removeUpdates(this);
+
+    }
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+            String lon = "lon="+String.valueOf(location.getLongitude());
+            String lat = "lat="+String.valueOf(location.getLatitude());
+
+            Log.i(LOG_TAG, url+"?"+lon+"&"+lat);
+            browserView.loadUrl(url+"?"+lon+"&"+lat);
+            locationFound();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
+
+/*all grouped together http://stackoverflow.com/questions/2352679/passing-gps-lonlat-from-android-gps-to-webpage-javascript
 public class FrankendealTrivia extends Activity implements LocationListener {
 
-    private static final String MAP_URL = "http://frankendeal.com/test/test2.php";
-    private WebView webView;
+    private static final String MAP_URL = "http://miniapps.co.uk/geometer/"; //"http://frankendeal.com/test/test2.php";
+//	private static final String MAP_URL = "http://frankendeal.com/test/test2.php";
+//	private static final String MAP_URL = "http://frankendeal.com/jstest.php";
+	
+	private WebView webView;
     private Location mostRecentLocation;
     private static final String LOG_TAG = "FrankenDeal";
 
     @Override
-    /** Called when the activity is first created. */
+    // Called when the activity is first created. 
     public void onCreate(Bundle savedInstanceState) {
         try {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.main);
             getLocation();
+
+           // webView.getSettings().setJavaScriptEnabled(true);
+          // webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+          //  webView.getSettings().setGeolocationEnabled(true);  //seems like if i set this, the webview should prompt when I call navigator.geolocation.getCurrentPosition
             setupWebView();
+            Log.d(LOG_TAG, "help me!");
             this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         } catch (Exception e) {
             e.printStackTrace();
@@ -89,13 +190,13 @@ public class FrankendealTrivia extends Activity implements LocationListener {
 
     }
 
-    /** Sets up the WebView object and loads the URL of the page **/
+    // Sets up the WebView object and loads the URL of the page 
     private void setupWebView() {
-        /*
+
             final String centerURL = "javascript:centerAt("
                     + mostRecentLocation.getLatitude() + ","
-                    + mostRecentLocation.getLongitude() + ")"; */
-
+                    + mostRecentLocation.getLongitude() + ")"; 
+            Log.d(LOG_TAG, (String ) centerURL);
             webView = (WebView) findViewById(R.id.webview);
             webView.getSettings().setJavaScriptEnabled(true);
 
@@ -113,11 +214,11 @@ public class FrankendealTrivia extends Activity implements LocationListener {
             webView.loadUrl(MAP_URL);
     }
 
-    /**
-     * The Location Manager manages location providers. This code searches for
-     * the best provider of data (GPS, WiFi/cell phone tower lookup, some other
-     * mechanism) and finds the last known location.
-     **/
+    //
+     // The Location Manager manages location providers. This code searches for
+    // the best provider of data (GPS, WiFi/cell phone tower lookup, some other
+    // mechanism) and finds the last known location.
+     
     private void getLocation() {
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
@@ -131,16 +232,21 @@ public class FrankendealTrivia extends Activity implements LocationListener {
         setMostRecentLocation(locationManager.getLastKnownLocation(provider));
     }
 
-    /** Sets the mostRecentLocation object to the current location of the device **/
+    public void locationFound(){
+
+        myLocationManager.removeUpdates(this);
+
+    }
+    // Sets the mostRecentLocation object to the current location of the device 
     @Override
     public void onLocationChanged(Location location) {
         setMostRecentLocation(location);
     }
 
-    /**
-     * The following methods are only necessary because WebMapActivity
-     * implements LocationListener
-     **/
+    //
+     // The following methods are only necessary because WebMapActivity
+     // implements LocationListener
+     //
     @Override
     public void onProviderDisabled(String provider) {
     }
@@ -153,16 +259,16 @@ public class FrankendealTrivia extends Activity implements LocationListener {
     public void onStatusChanged(String provider, int status, Bundle extras) {
     }
 
-    /**
-     * @param mostRecentLocation the mostRecentLocation to set
-     */
+    //
+     / @param mostRecentLocation the mostRecentLocation to set
+     //
     public void setMostRecentLocation(Location mostRecentLocation) {
         this.mostRecentLocation = mostRecentLocation;
     }
 
-    /**
-     * @return the mostRecentLocation
-     */
+    //
+     / @return the mostRecentLocation
+     //
     public double getMostRecentLocation(String lonLat) {
         double gValue = 0.00;
         try
@@ -179,6 +285,7 @@ public class FrankendealTrivia extends Activity implements LocationListener {
     }
 
 }
+all grouped together */
 
 /* jsilva 
 private class MyChromeWebViewClient extends WebChromeClient {
